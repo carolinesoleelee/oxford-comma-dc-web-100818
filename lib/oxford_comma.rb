6bd2033c2fd8d1f60351b6e8895a8bc98ff8f68e
Solee Lee <carolinesoleelee@gmail.com>
2018-09-26 17:52:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ def oxford_comma(array)
  if array.length == 1
    return array.join
  elsif array.length == 2
-  return array.push("and")
-  array << durian
+  return  array.insert(2, "and")
 else array.length > 2
   new = []
   array.each do |this|
