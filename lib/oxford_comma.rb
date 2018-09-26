3f07dd71d9451d04408900bf89e56ad274aede86
Solee Lee <carolinesoleelee@gmail.com>
2018-09-26 17:48:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
  if array.length == 1
    return array.join
  elsif array.length == 2
-  return array.join(" ")
+  return array.insert("and")
 else array.length > 2
   new = []
   array.each do |this|
